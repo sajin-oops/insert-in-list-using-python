eb5a26c5c64aef2060339347548d34b7adb86989
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-07-12 17:13:21 +0530

    Add files via upload

--- insert in list.py
+++ insert in list.py
@@ -2,3 +2,5 @@ cities = ["chennai","mumbai","delhi","maharastra"]
 print(cities)
 cities.insert(2,"bihar")
 print(cities)
+# after inserting the value
+#output  ['chennai', 'mumbai', 'bihar', 'delhi', 'maharastra']
